46df64ad3af2b1ef217ea42ddeee657fad0812b7
hanq11 <144857625+hanq11@users.noreply.github.com>
2024-09-25 14:08:24 +0700

    update buoi8 SD18403 java5

---
+++ SD18403.Java5/src/main/java/com/example/helloworldsd18403/buoi8/model/Staff.java
@@ -0,0 +1,19 @@
+package com.example.helloworldsd18403.buoi8.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Staff {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String position;
+    private String[] hobbies;
+    private Country country;
+}
